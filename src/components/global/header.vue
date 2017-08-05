f724f3d12b3efa6564f80a3d37a8a35b5cd393c4
Justin Helmer <justin.helmer@macys.com>
2017-08-04 23:15:12 -0700

    add a keystone link to the UI to go to the admin dash

--- src/components/global/header.vue
+++ src/components/global/header.vue
@@ -1,13 +1,24 @@
 <template>
     <header>
         <nav class="top-bar">
-            <ul class="menu">
-                <li>
-                    <router-link :to="{ name: 'home' }">
-                        <i class="fa fa-home"></i><span class="show-for-medium">Home</span>
-                    </router-link>
-                </li>
-            </ul>
+            <div class="top-bar-left">
+                <ul class="menu">
+                    <li>
+                        <router-link :to="{ name: 'home' }">
+                            <i class="fa fa-home"></i><span class="show-for-medium">Home</span>
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="top-bar-right">
+                <ul class="menu align-right">
+                    <li>
+                        <a href="/keystone">
+                            <i class="fa fa-cog"></i><span class="show-for-medium">Admin Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     </header>
 </template>
@@ -24,5 +35,11 @@
     $white: get-color(white);
 
     a {  color: $white;  }
-    i {  margin-right: 1em; }
+    i { margin-right: 1em; }
+
+    @include breakpoint(small only) {
+        .menu.align-right li:last-child i {
+            margin: 0;
+        }
+    }
 </style>
